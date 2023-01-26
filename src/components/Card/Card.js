08ac573522fbe82f362ccf5de3389f8bd9a721c8
Alexandra Podaru <podaru.alexandra96@yahoo.com>
2023-01-26 22:44:07 +0200

    Design update

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,24 @@
-
-
+import { faArrowUpFromBracket, faEllipsis, faHeart, faListDots } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Card.css";
 
 function Card(props) {
     const cat = props.cat;
-   
 
     return (
         <div className="Card_container">
             <img className="Card_img" src={cat?.url} alt="" />
+            <div className="Card_overlay">
+                <FontAwesomeIcon icon={faHeart} className="Card_favorite" />
+                <div className="Card_overlay_buttons_container">
+                    <div className="Card_overlay_buttons_background">
+                        <FontAwesomeIcon icon={faArrowUpFromBracket} />
+                    </div>
+                    <div className="Card_overlay_buttons_background">
+                        <FontAwesomeIcon icon={faEllipsis} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
